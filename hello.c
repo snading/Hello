4bdfcc88489deb8de5f84fe94fb4dc16ca01fb27
snading <snading2@huskers.unl.edu>
2022-08-25 21:07:15 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Sam Nading
+ * snading@unomaha.edu
+ * 2022/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+
+  printf("Sam Nading\n");
+
+  return 0;
+}
